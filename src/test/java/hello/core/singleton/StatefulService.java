a3e5ef6d1c12ea165a25fa7e313d969c81d54340
JUNGTAEK JOO <j3470@hotmail.com>
2021-01-10 23:05:15 +0900

    싱글톤 컨테이너

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,19 @@
+package hello.core.singleton;
+
+public class StatefulService {
+
+    private int price; // 상태 유지 코드
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
